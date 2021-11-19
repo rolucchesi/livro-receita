6a07977f627381177858c2127efe6b99513a5305
Rodrigo da Cunha Lucchesi <rlucchesi92@gmail.com>
2021-11-18 22:49:01 -0300

    Lógica do Header criada
    Ao clicar nas tabs do header, ele seleciona o componente apropriado para mostrar na tela

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-project';
+  tabSelected:string = 'recipes';
+
+  changeTab(tab:string){
+    this.tabSelected = tab;
+  }
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+    // showRecipesTab = true;
+    // showShoplistTab = false;
+
+    @Output() tabSelectFired = new EventEmitter<string>()
+
+    constructor(){
+
+    }
+
+    onSelect(tab:string){
+        this.tabSelectFired.emit(tab)
+    }
+
 
 }
